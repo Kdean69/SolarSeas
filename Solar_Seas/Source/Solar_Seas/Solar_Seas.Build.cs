93d36b38541ceb9a192fc8d2758034dcffd28486
Dean Livingstone <Dean.Livingstone@KdeanGames.com>
2021-12-06 13:41:26 -0500

    Base project setup

--- Solar_Seas/Source/Solar_Seas/Solar_Seas.Build.cs
+++ Solar_Seas/Source/Solar_Seas/Solar_Seas.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright © 2021 Kdean Games. All Rights Reserved.
 
 using UnrealBuildTool;
 
--- Solar_Seas/Source/Solar_Seas/Solar_Seas.cpp
+++ Solar_Seas/Source/Solar_Seas/Solar_Seas.cpp
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright © 2021 Kdean Games. All Rights Reserved.
 
 #include "Solar_Seas.h"
 #include "Modules/ModuleManager.h"
